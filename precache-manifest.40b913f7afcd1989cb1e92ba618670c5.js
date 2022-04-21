e4c4fcbc2da9d28b636e5668d8ab54241ae123d7
ErfanRht <erfanrht1384.com@gmail.com>
2022-04-22 00:43:49 +0430

    Updates

---
+++ precache-manifest.40b913f7afcd1989cb1e92ba618670c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcdd77f3143fd2b91fc4fa064f3cf95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0786fb7a7b30541e4994",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1390d13147fcc8767529",
+    "url": "/static/js/2.95a996af.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.95a996af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0786fb7a7b30541e4994",
+    "url": "/static/js/main.b015503f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/ErfanRahmati.32f6b574.png"
+  },
+  {
+    "revision": "e4f987da783975242075a89f425bd280",
+    "url": "/static/media/resume.e4f987da.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.0f6bc43019ff59ef5f5ccea3e3262873.js"
+  "/precache-manifest.40b913f7afcd1989cb1e92ba618670c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
